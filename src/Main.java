638c5ba678e96209ba655736abd1c98fb1fbfa28
Nesredin Ahmed <ahmednesredin@icloud.com>
2025-04-16 11:36:15 -0700

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //3x3 board
+        char[][] rows = {
+                {'.','.','.'}, // row 0
+                {'.','.','.'}, // row 1
+                {'.','.','.'}, // row 2
+                //0   1   2 positions in a row
+        };
+        rows[1][1] = 'X';
+
+        //DISPLAY THE WHOLE BOARD
+        for(int r = 0; r < rows.length; r++){
+            for(int c = 0; c < rows[r].length; c++) {
+                System.out.print(rows[r][c]); //on character - stacked left to right
+                System.out.print(' ');
+            }
+            System.out.print("\n"); //new line
         }
     }
 }
